90c646abaa32a7ed7c1856e7de5c8ed027952359
Daniil Chemaev <chemaev.ds@gmail.com>
2023-03-17 11:54:23 +0300

    Added User Profile page

--- webapp/src/main/java/com/chemaev/config/SecurityConfig.java
+++ webapp/src/main/java/com/chemaev/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
-                .antMatchers("/home")
+                .antMatchers("/home", "/user_profile")
                 .authenticated()
                 .anyRequest().permitAll()
                 .and()
--- webapp/src/main/java/com/chemaev/controllers/MainController.java
+++ webapp/src/main/java/com/chemaev/controllers/MainController.java
@@ -1,9 +1,12 @@
 package com.chemaev.controllers;
 
 import com.chemaev.dto.university.CreateUserRequestDto;
+import com.chemaev.model.university.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class MainController {
@@ -22,5 +25,12 @@ public class MainController {
     public String signUp(Model model) {
         model.addAttribute("user", new CreateUserRequestDto());
         return "sign_up";
+    }
+
+    @GetMapping("/user_profile")
+    public String userProfile(Model model, HttpServletRequest httpServletRequest) {
+        String currentPrincipalName = httpServletRequest.getUserPrincipal().getName();
+        model.addAttribute("user", currentPrincipalName);
+        return "user_profile";
     }
 }
